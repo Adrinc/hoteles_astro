fa605c9724ed1a7e9655a70118a7ef5ed235f9e3
Adrinc <abrahamd@cbluna.com>
2023-12-25 21:35:40 -0800

    primer commit

---
+++ src/components/react_components/RouterLinks.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter } from 'react-router-dom'
+
+import TopMenu2 from './TopMenu';
+
+function RoterLinks() {
+  return (
+    <BrowserRouter>
+      <TopMenu2 />
+    </BrowserRouter>
+  );
+}
+
+export default RoterLinks;
---
+++ tailwind.config.mjs
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {      
+			fontFamily: {
+			sans: ["InterVariable", "Inter"],
+		  },
+		  colors: {
+			primaryColor: 'var(--primary-color)',
+			secondaryColor: 'var(--secondary-color)',
+			primaryTextColor: 'var(--primary-text-color)',
+			secondaryTextColor: 'var(--secondary-text-color)',
+			tertiaryTextColor: 'var(--tertiary-text-color)',
+			quaternaryTextColor: 'var(--quaternary-text-color)',
+			primaryButtomColor: 'var(--primary-buttom-color)',
+			primaryButtomTextColor: 'var(--primary-buttom-text-color)',
+			secondaryButtomTextColor: 'var(--secondary-buttom-text-color)',
+			secondaryButtomColor: 'var(--secondary-buttom-color)',
+
+			navBarBgColor: 'var(--navbar-bg-color)',
+			navBarBlur: 'var(--navbar-blur)',
+
+			primaryBg: 'var(--primary-bg-color)',
+			secondaryBg: 'var(--secondary-bg-color)',
+			primaryGradient: 'var(--primary-gradient)',
+			secondaryGradient: 'var(--secondary-gradient)',
+			tertiaryGradient: 'var(--tertiary-gradient)',
+		  },
+		},
+	},
+	plugins: [ require('tailwindcss-animated')],
+}
